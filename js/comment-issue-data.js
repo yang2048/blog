590e7d614422c6165e1d310930769216ecf63084
unknown <yang2048@qq.com>
2020-05-04 18:17:40 +0800

    Site updated: 2020-05-04 18:17:39

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -331,7 +331,7 @@ function loadIssueData(appId, appKey, userName, userRepo, isValine) {
                 }
             }
         }
-        console.clear();
+        // console.clear();
         // console.log("~~~~欢迎光临！记得有时间多来看看哦，https://removeif.github.io/ ~~~~")
     }
         ,
